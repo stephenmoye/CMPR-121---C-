54e593a0c459476fd3633d385c6578bb3cdc4a51
Stephen Moye <stephenmoye@gmail.com>
2021-08-25 23:14:07 -0700

    HW_1a complete

--- Homework/HW_1/HW_1/Source.cpp
+++ Homework/HW_1/HW_1/Source.cpp
@@ -4,14 +4,13 @@
 //  File:   HW-1a
 // ===========================================================
 //   Programmer:   Stephen Moye
-//   Class:   CMPR 121 Tuesday
+//   Class:        CMPR 121 Tuesday
 //   Instructor:   Dennis Rainey
 //
 //   Description:
 //	 This function calculates the amount of a sale, the 
 //   commission of the sale, and the total pay.
 // =========================================================== 
-
 
 #include <iostream>
 #include <iomanip>
@@ -23,8 +22,11 @@ int calcCommission(float sales);
 int calcPay(float commission);
 void displayPay(float sales, float commission, float total);
 
-const int BASE = 2500.00;
+const float BASE_PAY = 2500.00;
 
+//  =============================================================================
+//  main
+//  =============================================================================
 int main() {
     float sales = 0;
     float commission = 0;
@@ -36,56 +38,104 @@ int main() {
     displayPay(sales, commission, total);
 
     return 0;
-
 }
+//  =============================================================================
+//  end of main
+//  =============================================================================
+
+
+
+//  =============================================================================
+//  getSalesAmt
+//  =============================================================================
+//	This function prompts the user to enter their monthly sales 
+// 
+//  Input: 
+//      no parameters
+//  Output: 
+//      the dollar amount of sales as a float and returns it to main()
+//  =============================================================================
 
 int getSalesAmt()
 {
-    int sales;
+    float sales;
 
     cout << "Enter monthly sales amount:\t" << endl;
     cin >> sales;
 
     return sales;
 }
+//  =============================================================================
+//  end of getSalesAmt
+//  =============================================================================
 
+
+
+//  =============================================================================
+//  calcCommission
+//  =============================================================================
+//  This function takes the value of sales and tests it against 3 commission tiers
+//  that determine the percentage of commission from the calculation.
+// 
+//  Input:
+//      sales - the dollar amount in sales, passed from main()
+//  Output:
+//      the commission amount is calculated by a percentage of the sales amount
+//      and returned to main() 
+//  =============================================================================
 int calcCommission(float sales)
 {
     int commission;
+
     if (sales > 50000)
-    {
         commission = sales * .02;
-    }
     else if (sales >= 25000 && sales <= 50000)
-    {
         commission = sales * .015;
-    }
     else
-    {
         commission = 0;
-    }
 
     return commission;
 }
+//  =============================================================================
+//  end of calcCommission
+//  =============================================================================
 
+
+
+//  =============================================================================
+//  calcPay
+//  =============================================================================
+// 
+//  Input:
+//      sales - the dollar amount in sales, passed from main()
+//  Output:
+//      the commission amount is calculated by a percentage of the sales amount
+//      and returned to main() 
+//  =============================================================================
 int calcPay(float commission)
 {
     float total;
-    total = commission + BASE;
+    total = commission + BASE_PAY;
 
     return total;
 }
+//  =============================================================================
+//  end of calcPay
+//  =============================================================================
+
+
+
 
 void displayPay(float sales, float commission, float total)
 {
     cout << "Monthly sales:\t $ " << setprecision(2) << fixed << sales << endl << endl;
     cout << "Commission:\t $ " << setprecision(2) << commission << endl << endl;
-    cout << "Base Pay:\t $ " << setprecision(2) << BASE << endl << endl;
+    cout << "Base Pay:\t $ " << setprecision(2) << BASE_PAY << endl << endl;
     cout << "Total Pay:\t $ " << setprecision(2) << total << endl << endl;
 
     char again;
     
-    cout << "Do it again? (Y/N)";
+    cout << "Do it again? (Y/N) ";
     cin >> again;
 
     if (toupper(again) == 'Y')
